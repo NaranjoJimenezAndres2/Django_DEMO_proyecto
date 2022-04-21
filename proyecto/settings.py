5832eb88324cb38a46066fa132f5e808b75fdb13
andresASIR <andres.narjim@gmail.com>
2022-04-21 21:41:41 +0200

    first commit

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-(f!gc7&yt$sq3h8$g%ihj$jx2kje)++$ft-hbt&@(*c5tvdoxq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
